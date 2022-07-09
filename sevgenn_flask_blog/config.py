3c02dba8f6c4b4f9072bec3ca75e6d132dc66a73
sevgenn <sevgenn@gmail.com>
2022-07-10 00:06:08 +0300

    config

--- sevgenn_flask_blog/config.py
+++ sevgenn_flask_blog/config.py
@@ -3,9 +3,9 @@ class Configuration:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SECRET_KEY = '49905f00de97b0669fe7e4e87f3d3d05'
 
-    # MAIL_SERVER = 'smtp.yandex.ru'
-    # MAIL_PORT = 465
-    # MAIL_USE_TLS = False
-    # MAIL_USE_SSL = True
-    # MAIL_USERNAME = 'jsdoe@yandex.ru'
-    # MAIL_PASSWORD = 'testinG77'
+    MAIL_SERVER = 'smtp.yandex.ru'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'jsdoe@yandex.ru'
+    MAIL_PASSWORD = 'vARIANt_77'
