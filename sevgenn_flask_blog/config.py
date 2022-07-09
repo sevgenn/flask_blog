15077f6be8f609ddba1daa47ed53cceb223dd316
sevgenn <sevgenn@gmail.com>
2022-07-10 00:57:53 +0300

    config.py

--- sevgenn_flask_blog/config.py
+++ sevgenn_flask_blog/config.py
@@ -4,7 +4,7 @@ import os
 class Configuration:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SECRET_KEY = os.getenv('SECRET_KEY', '49905f00de97b0669fe7e4e87f3d3d05')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'something very secret')
 
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = os.getenv('MAIL_PORT')
